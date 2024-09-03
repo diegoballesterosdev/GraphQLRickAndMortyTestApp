9c93fd87f0568644a57531fc70a3c3db3bd49e67
diegoballesterosdev <diegoballesteros.dev@gmail.com>
2024-09-03 12:08:17 -0600

    Initial Commit

---
+++ GraphQLRickAndMortyTestApp/GraphQLRickAndMortyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLRickAndMortyTestAppApp.swift
+//  GraphQLRickAndMortyTestApp
+//
+//  Created by Diego Ballesteros on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLRickAndMortyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
